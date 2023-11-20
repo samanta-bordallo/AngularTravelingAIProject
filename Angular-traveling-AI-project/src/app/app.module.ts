cab4e773edf7593c8231152aaeb2ea3170e5af16
victor-schumann <schumann@tutamail.com>
2023-11-20 17:22:07 +0000

    add input description

--- Angular-traveling-AI-project/src/app/app.module.ts
+++ Angular-traveling-AI-project/src/app/app.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
-
 import { AppComponent } from './app.component';
-import { SecondScreenComponent } from './second-screen/second-screen.component';
 import { FirstScreenComponent } from './first-screen/first-screen.component';
 import { RouterModule } from '@angular/router';
 import { ThirdScreenComponent } from './third-screen/third-screen.component';
-
+import { SecondScreenComponent } from './second-screen/second-screen.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 
 @NgModule({
@@ -16,15 +17,22 @@ import { ThirdScreenComponent } from './third-screen/third-screen.component';
     AppComponent,
     SecondScreenComponent,
     FirstScreenComponent,
-    ThirdScreenComponent
+    ThirdScreenComponent,
+    AppComponent,
+
   ],
   imports: [
     BrowserModule,
     FormsModule,
     RouterModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
+
 export class AppModule { }
